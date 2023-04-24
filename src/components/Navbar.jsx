14e434eb36d8fc13ee397bb24b2958cf21a05656
Fonlogen (PC) <info.fonlogen@gmail.com>
2023-04-25 01:21:53 +0200

    Changed "Login" text to "Account"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import Logo from '../assets/obo-soft-orange.png'
 
 function Navbar(props) {
     return (
-        <div className="flex flex-row w-full p-3 h-30 items-center justify-between">
+        <div className="navbar">
             <img src={Logo} alt='logo' className='w-30 h-10' />
-            <div className='w-fit py-1 px-7 text-center items-center text-black text-2xl rounded-xl cursor-pointer hover:text-orange-600'>
-                {
-                    props.name !== false ? props.name : 'Login'
-                }
+            <div className='w-fit py-1 px-7 text-center items-center text-black text-2xl rounded-xl cursor-pointer hover:text-orange-600 hidden sm:flex flex-row gap-2'>
+                <span>{
+                    props.name !== false ? props.name : 'Account' 
+                }</span>
             </div>
         </div>
     )
