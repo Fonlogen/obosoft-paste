aee56de0d56877d303d909cf958041b7eba99b31
Fonlogen (PC) <info.fonlogen@gmail.com>
2023-04-25 01:22:52 +0200

    Cleaned and added references to pages and comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 
+// Components
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+
 function App() {
   return (
     <>
-      <div>
-        <Router>
-          <Routes>
-            <Route path="/" element={<h1>Home</h1>} />
-            <Route path="dashboard" element={<h1>Account</h1>} />
-            <Route path="*" element={<h1>404</h1>} />
-          </Routes>
-        </Router>
-      </div>
+      <Navbar name={false} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="dashboard" element={<h1>Account</h1>} />
+          <Route path="*" element={<h1>404</h1>} />
+        </Routes>
+      </Router>
     </>
   )
 }
