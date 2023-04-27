f7f5c6e6d0577efaad7041f6adb60372b269a751
Fonlogen (PC) <info.fonlogen@gmail.com>
2023-04-27 17:52:37 +0200

    Added a state to check if user is logged in, WIP

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,50 @@ import './App.css'
 
 // Components
 import Navbar from './components/Navbar'
+import NotFound from './pages/NotFound'
 import HomePage from './pages/HomePage'
+import AccountPage from './pages/AccountPage'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+
+import { useState, useEffect } from 'react'
+
+import { Client, Account } from "appwrite";
+
+const client = new Client();
+
+const account = new Account(client);
+
+client
+    .setEndpoint('https://obosoft.it:8056/v1') // Your API Endpoint
+    .setProject('6441c6e7d6448edcc109') // Your project ID
+;
 
 function App() {
+  // const [userAccount, setUserAccount] = useState(false)
+  const [userAccount, setUserAccount] = useState(false)
+
+  useEffect(() => {
+    const promise = account.get();
+
+    promise.then(function (response) {
+      setUserAccount(response)
+    }, function () { // Error
+      setUserAccount(false)
+    });
+  }, [])
+
   return (
     <>
-      <Navbar name={false} />
+      <Navbar/>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="dashboard" element={<h1>Account</h1>} />
-          <Route path="*" element={<h1>404</h1>} />
+          <Route path="/" element={<HomePage account={userAccount} />} />
+          <Route path="dashboard" element={<AccountPage account={userAccount} setAccount={setUserAccount} />} />
+          <Route path="login" element={<Login account={userAccount} setAccount={setUserAccount} />} />
+          <Route path="register" element={<Register account={userAccount} setAccount={setUserAccount} />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </>
